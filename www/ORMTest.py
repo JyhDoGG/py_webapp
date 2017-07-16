c160fe0b969abb74dee4d0c524a9bb3fa489f318
DoGG <875134940@qq.com>
2017-07-16 19:25:25 +0800

    modify ORMTest.py

--- www/ORMTest.py
+++ www/ORMTest.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 #coding=utf-8
 
-from ORM import Model, StringField, IntegerField
+import ORM
+from models import User, Blog, Comment
 
-class User(Model):
-    __table__ = 'users'
-    id = IntegerField(primary_key=True)
-    name = StringField()
+def test():
+    yield from ORM.create_pool(user='blogadmin', password='jyh001', database='blog_app')
 
-user=User(id=123,name='Jyh')
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
 
+    yield from u.save()
+
+for x in test():
+    pass
